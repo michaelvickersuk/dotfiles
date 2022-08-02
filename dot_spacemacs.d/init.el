818749ce77a7d2894ad903a956268a8c0c28d768
Michael Vickers <michael.vickers@joslinrhodes.co.uk>
2022-08-02 07:17:17 +0100

    Set global whitespace mode and customisations

--- dot_spacemacs.d/init.el
+++ dot_spacemacs.d/init.el
@@ -570,6 +570,30 @@ configuration.
 Put your configuration code here, except for variables that should be set
 before packages are loaded."
    (editorconfig-mode 1)
+
+   (global-whitespace-mode)
+
+   (set-face-attribute 'whitespace-newline nil
+     :background nil
+     :foreground "gray30")
+
+   (set-face-attribute 'whitespace-space nil
+     :background nil
+     :foreground "gray30")
+
+     (progn
+      ;; Make whitespace-mode with very basic background coloring for whitespaces.
+       ;; http://xahlee.info/emacs/emacs/whitespace-mode.html
+       (setq whitespace-style (quote (face spaces tabs newline space-mark tab-mark newline-mark )))
+
+       ;; Make whitespace-mode and whitespace-newline-mode use “¶” for end of line char and “▷” for tab.
+       (setq whitespace-display-mappings
+             ;; all numbers are unicode codepoint in decimal. e.g. (insert-char 182 1)
+             '(
+               (space-mark 32 [183] [46]) ; SPACE 32 「 」, 183 MIDDLE DOT 「·」, 46 FULL STOP 「.」
+               (newline-mark 10 [8629 10]) ; LINE FEED,
+               (tab-mark 9 [9655 9] [92 9]) ; tab
+               )))
 )
 
 
