ba723ccf9354c3081360e9ea98341cbd2558c94c
Michael Vickers <michael.vickers@joslinrhodes.co.uk>
2023-04-18 13:40:47 +0100

    Add JSON package

--- dot_spacemacs.d/init.el
+++ dot_spacemacs.d/init.el
@@ -45,6 +45,7 @@ This function should only modify configuration layer settings."
      (html :variables html-enable-lsp t)
      helm
      (javascript :variables js2-mode-show-strict-warnings nil)
+     json
      (lsp :variables lsp-file-watch-threshold 3000)
      markdown
      multiple-cursors
