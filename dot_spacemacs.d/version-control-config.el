bf5bd6d370935d1fc3eb5a795a818dc5e52218df
Michael Vickers <michael.vickers@joslinrhodes.co.uk>
2023-07-28 13:30:32 +0100

    Add visit new pull request function
    Originates from https://endlessparentheses.com/create-github-prs-from-emacs-with-magit.html

--- dot_spacemacs.d/version-control-config.el
+++ dot_spacemacs.d/version-control-config.el
@@ -4,3 +4,17 @@
 ;; Version Control configuration
 ;; ---------------------------------------
 
+(defun visit-new-pull-request-url ()
+  "Create a pull request for the current branch on GitHub"
+  (interactive)
+  (browse-url
+   (format "https://github.com/%s/pull/new/%s"
+           (replace-regexp-in-string
+            "\\`.+github\\.com:\\(.+\\)\\.git\\'"
+            "\\1"
+            (magit-get "remote" (magit-get-push-remote) "url"))
+           (magit-get-current-branch))))
+
+(eval-after-load
+    'magit '(define-key magit-mode-map "v" #'visit-new-pull-request-url))
+
